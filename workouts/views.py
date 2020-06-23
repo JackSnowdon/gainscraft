2e0a05c7b800e637f2ea82d01f861c43ef5b47de
JackSnowdon <jacksnowdondrums@gmail.com>
2020-06-23 17:22:48 +0100

    first draft of working with squat data

--- workouts/views.py
+++ workouts/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from .models import *
+from datetime import date, datetime, timedelta
 
 # Create your views here.
 
 def workout_home(request):
     profile = request.user.profile
     squats = Squat.objects.filter(done_by=profile)
-    return render(request, "workout_home.html", {"squats": squats})
+    t = date.today()
+    squats_today = []
+    for s in squats:
+        if s.done_on.date() == t:
+            squats_today.append(s)
+    return render(request, "workout_home.html", {"squats": squats, "squats_today": squats_today})
