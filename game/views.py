52352fa606fbbdf53b1c65cf768cc7aa0db81fae
JackSnowdon <jacksnowdondrums@gmail.com>
2020-08-03 13:35:48 +0100

    base bonus and point display for game

--- game/views.py
+++ game/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from workouts.views import return_single_day, return_day_list, return_day_values
 from workouts.models import *
+import math
 
 # Create your views here.
 
@@ -15,16 +16,19 @@ def game_home(request):
     t = date.today()
     day_list = return_day_list(t, start_date)
     point_dict = return_day_values(day_list, workouts)
-    point_total = 0
+    point_base = 0
     current_streak = 0
-    longest_streak = 0 
+    longest_streak = 0
+    streak_bonus = 0
     for day, value in point_dict.items():
-        point_total += value
         if value > 0:
             current_streak += 1
+            streak_bonus += math.floor(value * 1.125) - value
         else:
             if current_streak > longest_streak:
                 longest_streak = current_streak
             current_streak = 0
-    return render(request, "game_home.html", {"point_total": point_total})
+        point_base += value
+    point_total = point_base + streak_bonus
+    return render(request, "game_home.html", {"point_base": point_base, "longest_streak": longest_streak, "point_total": point_total, "streak_bonus": streak_bonus, "start_date": start_date })
 
